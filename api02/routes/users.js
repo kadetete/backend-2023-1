3a1f171785eab1f1007f21fa984ab621932d7a6f
kadetete <kadetesan@gmail.com>
2023-05-11 08:25:51 -0300

    mod router users

--- api02/routes/users.js
+++ api02/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/teste', function(req, res) {
+  res.send('Meu novo método muito do massa kkkkkkkkkkkkkkkkkkkkk')
+})
+
 module.exports = router;
