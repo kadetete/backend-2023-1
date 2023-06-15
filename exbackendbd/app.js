790523396ece464b61ca719a0ab645bc37bdfd61
kadetete <kadetesan@gmail.com>
2023-06-15 08:30:34 -0300

    implementando metodo login

--- exbackendbd/app.js
+++ exbackendbd/app.js
@@ -3,8 +3,10 @@ const mysql = require('mysql');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
-app.use(cors);
+app.use(cors());
+app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -21,15 +23,30 @@ connection.connect(function(err) {
     console.log('Conectado ao banco de dados!')
 });
 
-
+function gerarToken(payload) {
+    const senhaToken = 'IFRN2@23';
+    return jwt.sign(payload, senhaToken, {expiresIn: 20});
+}
 
 app.post('/login', function(req, res) {
-    connection.query('SELECT codusuarios, nomeusuario, loginname FROM usuarios', function(err, rows) {
+    const loginname = req.body.loginname;
+    const password = req.body.password;
+    connection.query('SELECT nomeusuario FROM usuarios WHERE loginname = ? AND password = ?',
+    [loginname, password], function(err, rows) {
         if (err) {
             console.error('Erro ao executar a consulta:', err);
             return;
         }
-        res.json(rows);
+        else {
+            if (rows.length > 0) {
+                const payload = { nomeusuario: rows[0].nomeusuario };
+                const token = gerarToken(payload);
+                res.json({ acessToken: token });
+            }
+            else {
+                res.status(403).json({ mensagemErro: 'Usuario ou senha inválidos' });
+            }   
+        }       
     });
 });
 
